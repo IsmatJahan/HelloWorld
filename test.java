3d11f63008d4c72c78264208847cd0f997836311
IsmatJahan <44657965+IsmatJahan@users.noreply.github.com>
2019-11-15 06:55:13 -0600

    Changed test.java and Post Build config

--- test.java
+++ test.java
@@ -2,6 +2,8 @@
 public class test{
 public static void main(String[] args){
 
-for (int i=1; i<=12;i++){
+for (int i=1; i<=18;i++){
+System.out.println("GIT _ JENKINS _ JAVA');
 System.out.println("Hello World....."+i);
+
 }}}
