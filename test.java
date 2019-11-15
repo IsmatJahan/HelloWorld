3cd45560060fda74441b7be87ab9deafee05c7ac
IsmatJahan <44657965+IsmatJahan@users.noreply.github.com>
2019-11-15 07:00:50 -0600

    Changed tes.java and Post Build in Jenkins

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ public class test{
 public static void main(String[] args){
 
 for (int i=1; i<=18;i++){
-System.out.println("GIT _ JENKINS _ JAVA');
+System.out.println("GIT _ JENKINS _ JAVA");
 System.out.println("Hello World....."+i);
 
 }}}
