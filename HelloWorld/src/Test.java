81f5d1457c885b3502c2713c8a1bae609d2059a2
IsmatJahan <44657965+IsmatJahan@users.noreply.github.com>
2019-11-15 08:12:09 -0600

    3rd Commit : Edited Test.java

--- HelloWorld/src/Test.java
+++ HelloWorld/src/Test.java
@@ -1,8 +1,14 @@
 
 public class Test {
 
-// This a 2nd addition to my Hello World GIT/Jenkins/Eclipse tutorial
+public static void main (String[] args) {// This a 2nd addition to my Hello World GIT/Jenkins/Eclipse tutorial
 	
-//System.out.println("This is another class added to my Heloo world project:  This is GIT/ECLIPSE/JENKINS Tutorial");
+
+		for (int i=1; i<=18;i++){
+		System.out.println("Hello World....."+i);
+
+		}
 	
+System.out.println("This is another class added to my Hello World project:  This is GIT/ECLIPSE/JENKINS Tutorial");
+}
 }
