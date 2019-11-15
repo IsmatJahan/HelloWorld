8306c6ea94e4aa6cb58dd72f7e332f6cdbb090e4
IsmatJahan <44657965+IsmatJahan@users.noreply.github.com>
2019-11-15 08:00:06 -0600

    2nd Commit: Added Test.java class

---
+++ HelloWorld/src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+
+// This a 2nd addition to my Hello World GIT/Jenkins/Eclipse tutorial
+	
+//System.out.println("This is another class added to my Heloo world project:  This is GIT/ECLIPSE/JENKINS Tutorial");
+	
+}
